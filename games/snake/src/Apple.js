40d542fb61a5875a9a6a149b96196a7dc229c6ba
NukeZ <curtis.jah@gmail.com>
2016-03-04 19:13:00 -0800

    finished scaling main and head have scale hardcoded

--- games/snake/src/Apple.js
+++ games/snake/src/Apple.js
@@ -1,8 +1,9 @@
-function Apple($el) {
+function Apple($el, size) {
   this.node = $('<img id="apple"></img>');
   this.node.attr('src', 'games/snake/src/assets/apple.jpg');
-  this.x = Math.floor(Math.random() * 14)
-  this.y = Math.floor(Math.random() * 14);
+  this.node.css({'width': 50 * size, 'height': 50 * size});
+  this.x = Math.floor(Math.random() * 10);
+  this.y = Math.floor(Math.random() * 10);
   $el.append(this.node);
   this.render();
 }
@@ -11,4 +12,4 @@ Apple.prototype.render = Head.prototype.render;
 
 Apple.prototype.eat = function() {
   this.node.remove();
-}
+};
--- games/snake/src/Body.js
+++ games/snake/src/Body.js
@@ -1,5 +1,6 @@
-function Body($el, x, y) {
+function Body($el, x, y, size) {
   this.node = $('<div class="body"></div>');
+  this.node.css({'width': 50 * size, 'height': 50 * size});
   $el.append(this.node);
   this.x = x;
   this.y = y;
