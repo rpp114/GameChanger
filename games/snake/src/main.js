6d01bad3f194f50f5de017f48d2565cbea66e7fb
Joshua Apstein <josh4locations@gmail.com>
2016-03-28 09:51:07 -0700

    phaser stuff

--- games/snake/src/main.js
+++ games/snake/src/main.js
@@ -211,7 +211,7 @@ $(document).ready(function() {
                 }
               } else {
                 head.isPaused = !head.isPaused;
-                head.move();
+                head.move(); 
               }
               if(head.currentDirection !== dirs[swipedir]) {
                 head.counter++;
